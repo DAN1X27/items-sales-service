a039cd7584953eac64613007d14376f25f78fe8c
Daniil <danikcheban2007@gmail.com>
2025-05-06 14:29:44 +0300

    Added authentication for eureka-server

--- eureka_server/src/main/java/danix/app/eureka_server/EurekaServerApplication.java
+++ eureka_server/src/main/java/danix/app/eureka_server/EurekaServerApplication.java
@@ -3,12 +3,29 @@ package danix.app.eureka_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableWebSecurity
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
 }
