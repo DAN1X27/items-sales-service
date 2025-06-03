6d3fff0c47b18329c2f7645abe547b34af5bce51
Daniil <danikcheban2007@gmail.com>
2025-06-03 20:50:20 +0300

    Add login form for eureka server and config server

--- config-server/src/main/java/danix/app/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/danix/app/config_server/ConfigServerApplication.java
@@ -3,28 +3,13 @@ package danix.app.config_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableWebSecurity
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
-                .build();
     }
 
 }
