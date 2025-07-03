a99d51ec0bcf0ca5c219705c6083fff7fc912233
Daniil <danikcheban2007@gmail.com>
2025-07-04 02:33:56 +0300

    Add tests in authentication-service

--- authentication-service/src/main/java/danix/app/authentication_service/dto/LoginDTO.java
+++ authentication-service/src/main/java/danix/app/authentication_service/dto/LoginDTO.java
@@ -2,12 +2,14 @@ package danix.app.authentication_service.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class LoginDTO {
 
 	@NotBlank(message = "Username must not be empty")
